8c9be8a9a36da959e55b6bac553d71373d7e3d98
Alexander <mrreads@yandex.com>
2020-02-22 19:08:23 +0300

    text format

--- index.php
+++ index.php
@@ -8,10 +8,13 @@
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, rerum perferendis? Quisquam ut quaerat modi aperiam eveniet, necessitatibus veritatis nam atque eos explicabo minus, beatae, cupiditate natus inventore a dolorum.
                     Consequuntur ipsa animi aperiam! Esse nam, minus enim facere ratione magnam atque quidem? Laudantium eos voluptatem eaque atque? Voluptate pariatur tempora sapiente, deserunt assumenda natus nesciunt consequuntur ducimus aperiam et.
                     In assumenda, autem repudiandae praesentium laudantium animi cumque odio! Nam omnis eos labore sed vitae quo error possimus repellat, voluptates eligendi voluptatem molestiae minima doloremque voluptas pariatur provident tempore similique!
-                    Possimus delectus, cupiditate ipsum nam, facere tenetur, provident dicta doloremque sit porro reprehenderit fugiat. Ab consectetur reiciendis explicabo rem amet quisquam ipsa quos. Delectus unde suscipit ipsam architecto impedit amet?
+                    Possimus delectus, cupiditate ipsum nam, facere tenetur, provident dicta doloremque sit porro reprehenderit fugiat. 
+                    <br> <br>
+                    Ab consectetur reiciendis explicabo rem amet quisquam ipsa quos. Delectus unde suscipit ipsam architecto impedit amet?
                     Rem dicta ut, laborum quidem repudiandae similique, minima repellendus sit fuga, explicabo dolores fugiat modi consequatur eligendi iure totam nobis accusantium dolor ducimus ipsa labore harum sint ex reprehenderit! Dicta!
                     Distinctio laboriosam quo dolor aliquid autem facere consequuntur odio repudiandae voluptas! Ipsam explicabo mollitia provident, totam quos ipsa, reiciendis eos tempora culpa perspiciatis voluptatum distinctio aut voluptates vero debitis illum.
                     Id ad, facere impedit cupiditate vel aut odit iusto atque neque obcaecati corporis tenetur distinctio accusamus nesciunt ipsa corrupti, aliquid excepturi magnam expedita eius officiis animi hic. Soluta, molestiae voluptas.
+                    <br> <br>
                     Adipisci, error quos asperiores, veritatis voluptas recusandae dolorem voluptatibus rem corporis doloribus corrupti maiores. Rerum nam, in harum esse id beatae natus non reprehenderit incidunt quas culpa explicabo, dolorum inventore.
                     Similique eligendi nam vero impedit, atque exercitationem sapiente ipsam, reprehenderit repellat a rem minus fugit fuga quis autem consequuntur mollitia et ab explicabo accusantium? Optio, debitis! Ipsa totam tenetur ipsum.
                     Quas incidunt amet ab corporis hic accusamus vel vitae impedit dolorem? Iusto veniam magnam tempora ad quidem, voluptate ea at consequuntur, nam qui aspernatur. Impedit quo molestiae laborum repudiandae doloribus.
@@ -25,7 +28,7 @@
                         </div>
                         <hr>
                         <div class="n-text">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia eaque doloribus velit ex quidem dicta. Tenetur quia labore pariatur dicta et velit nemo quam accusantium a, quibusdam quos quisquam non.Quo veniam asperiores rerum perferendis assumenda animi, sapiente quasi dolorum blanditiis nulla inventore alias sed minus consequuntur molestias omnis. Minima explicabo mollitia nostrum excepturi provident sit numquam illum. Quas, at.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia eaque doloribus velit ex quidem dicta. Tenetur quia labore pariatur dicta et velit nemo quam accusantium a, quibusdam quos quisquam non.</p>
                         </div>
                     </div>
                     <div class="news">
@@ -35,7 +38,7 @@
                         </div>
                         <hr>
                         <div class="n-text">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia eaque doloribus velit ex quidem dicta. Tenetur quia labore pariatur dicta et velit nemo quam accusantium a, quibusdam quos quisquam non.Quo veniam asperiores rerum perferendis assumenda animi, sapiente quasi dolorum blanditiis nulla inventore alias sed minus consequuntur molestias omnis. Minima explicabo mollitia nostrum excepturi provident sit numquam illum. Quas, at.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia eaque doloribus velit ex quidem dicta. Tenetur quia labore pariatur dicta et velit nemo quam accusantium a, quibusdam quos quisquam non.</p>
                         </div>
                     </div>
                     <div class="news">
@@ -45,7 +48,7 @@
                         </div>
                         <hr>
                         <div class="n-text">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia eaque doloribus velit ex quidem dicta. Tenetur quia labore pariatur dicta et velit nemo quam accusantium a, quibusdam quos quisquam non.Quo veniam asperiores rerum perferendis assumenda animi, sapiente quasi dolorum blanditiis nulla inventore alias sed minus consequuntur molestias omnis. Minima explicabo mollitia nostrum excepturi provident sit numquam illum. Quas, at.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia eaque doloribus velit ex quidem dicta. Tenetur quia labore pariatur dicta et velit nemo quam accusantium a, quibusdam quos quisquam non.</p>
                         </div>
                     </div>
                 </div>
