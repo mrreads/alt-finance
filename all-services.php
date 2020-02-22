b1a7f54526852cc5a5d5eb3cb290aed40ad2a00e
Alexander <mrreads@yandex.com>
2020-02-22 16:00:04 +0300

    fix bug

--- all-services.php
+++ all-services.php
@@ -23,12 +23,12 @@
                         </div>
                     </div>
                     <div class="swiper-slide">
-                        <a class="slider-item">                
+                        <div class="slider-item">                
                             <h1>Плавающий дом</h1>
                             <img src="https://avatars.mds.yandex.net/get-pdb/2126974/bec88ae6-f645-42b3-8478-e5018ad2bf12/s1200">
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, aperiam assumenda expedita est excepturi ullam perferendis corporis sed nostrum nam sint provident, quae dolores ducimus, repudiandae ex? Optio, consectetur.</p>
                             <a href="#" class="to-project-page">На страницу проекта</a>
-                        </a>
+                        </div>
                     </div>
                     <div class="swiper-slide">
                         <div class="slider-item">                
