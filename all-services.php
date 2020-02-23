ce4f78ebfeea188b249d05376f4b015196402324
Alexander <mrreads@yandex.com>
2020-02-24 01:26:27 +0300

    added slider

--- all-services.php
+++ all-services.php
@@ -5,44 +5,42 @@
     <section class="slider">
         <div class="container" style="padding-bottom: 0px;">
             <div class="swiper-container slider">
-            <h1 class="title">Новые проекты</h1>
+            <!-- <h1 class="title">Новые проекты</h1> -->
                 <div class="swiper-wrapper">
                     <div class="swiper-slide">
-                        <div class="slider-item">                
-                            <h1>Плавающий дом</h1>
-                            <img src="https://avatars.mds.yandex.net/get-pdb/2126974/bec88ae6-f645-42b3-8478-e5018ad2bf12/s1200">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, aperiam assumenda expedita est excepturi ullam perferendis corporis sed nostrum nam sint provident, quae dolores ducimus, repudiandae ex? Optio, consectetur.</p>
-                            <a href="#" class="to-project-page">На страницу проекта</a>
-                        </div>
+                        <a class="slider-item" href="/consalting.php" style="background-image: url('https://myrouble.ru/wp-content/uploads/2018/04/1-consulting-2.jpg')">
+                            <h2>КОНСАЛТИНГ</h2>
+                        </a>
                     </div>
                     <div class="swiper-slide">
-                        <div class="slider-item">                
-                            <h1>Плавающий дом</h1>
-                            <img src="https://avatars.mds.yandex.net/get-pdb/2126974/bec88ae6-f645-42b3-8478-e5018ad2bf12/s1200">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, aperiam assumenda expedita est excepturi ullam perferendis corporis sed nostrum nam sint provident, quae dolores ducimus, repudiandae ex? Optio, consectetur.</p>
-                            <a href="#" class="to-project-page">На страницу проекта</a>
-                        </div>
+                        <a class="slider-item" href="/building.php" style="background-image: url('https://rcmm.ru/uploads/posts/2016-11/1478692650_865637921.jpg')">
+                            <h2>ПРОЕКТИРОВАНИЕ, СТРОИТЕЛЬСТВО, РЕКОНСТРУКЦИЯ, ЭЛЕКТРОМОНТАЖНЫЕ РАБОТЫ, ИНЖИНИРИНГОВАЯ ДЕЯТЕЛЬНОСТЬ</h2>
+                        </a>
                     </div>
                     <div class="swiper-slide">
-                        <div class="slider-item">                
-                            <h1>Плавающий дом</h1>
-                            <img src="https://avatars.mds.yandex.net/get-pdb/2126974/bec88ae6-f645-42b3-8478-e5018ad2bf12/s1200">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, aperiam assumenda expedita est excepturi ullam perferendis corporis sed nostrum nam sint provident, quae dolores ducimus, repudiandae ex? Optio, consectetur.</p>
-                            <a href="#" class="to-project-page">На страницу проекта</a>
-                        </div>
+                        <a class="slider-item"  href="/autoVAZ.php"style="background-image: url('https://rossaprimavera.ru/static/files/38ef4d3f7b6a.jpg')">
+                            <h2>ПРОИЗВОДСТВО ТРАНСПОРТНЫХ СРЕДСТВ, ИНЖИНИРИНГОВАЯ ДЕЯТЕЛЬНОСТЬ</h2>
+                        </a>
                     </div>
                     <div class="swiper-slide">
-                        <div class="slider-item">                
-                            <h1>Плавающий дом</h1>
-                            <img src="https://avatars.mds.yandex.net/get-pdb/2126974/bec88ae6-f645-42b3-8478-e5018ad2bf12/s1200">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, aperiam assumenda expedita est excepturi ullam perferendis corporis sed nostrum nam sint provident, quae dolores ducimus, repudiandae ex? Optio, consectetur.</p>
-                            <a href="#" class="to-project-page">На страницу проекта</a>
-                        </div>
+                        <a class="slider-item" href="/IT.php" style="background-image: url('https://awsimages.detik.net.id/customthumb/2013/07/06/319/datacentre.jpg?w=700&q=90')">
+                            <h2>IT-ТЕХНОЛОГИИ, АВТОМАТИЗАЦИЯ ПРОИЗВОДСТВА, ИНЖИНИРИНГОВАЯ ДЕЯТЕЛЬНОСТЬ</h2>
+                        </a>
                     </div>
-                </div>
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>
+                    <div class="swiper-slide">
+                        <a class="slider-item" href="/science.php" style="background-image: url('https://yarkb3.ru/wp-content/uploads/2018/12/kafedra1.jpg')">
+                            <h2>НАУЧНО-ИССЛЕДОВАТЕЛЬСКАЯ ДЕЯТЕЛЬНОСТЬ</h2>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a class="slider-item" href="/geology.php" style="background-image: url('https://lh3.googleusercontent.com/proxy/cOK3R7U5ytAU0x-rHjj9GzDtCcx6hnhIC8uk2jBA-L2eg7i_7NnZA4CidRNQbpoHOwrLhMfi5T56sOLJnWHtijevVCL--OSz')">
+                            <h2>ИНЖЕНЕРНО-ГЕОЛОГИЧЕСКИЕ ИЗЫСКАНИЯ</h2>
+                        </a>
+                    </div> 
+                </div>  
             </div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
         </div>
     </section>
 
@@ -78,7 +76,6 @@
 
     <?php include_once ('footer.php');?>
 
-    <script type="text/javascript" src="/resources/js/swiper.min.js"></script>
     <script type="text/javascript" src="/resources/js/init-swiper.js"></script>
 
 
