e54c7d9f6b30605d3c84a116a6d6794eb2c17b70
Ilya <ilya.samarczew2011@yandex.ru>
2020-03-01 22:40:30 +0300

    Menu bug fix

--- wp-content/themes/alt-finance/assets/js/open-menu.js
+++ wp-content/themes/alt-finance/assets/js/open-menu.js
@@ -1,11 +1,10 @@
 document.querySelector('img.hidden-nav-button').addEventListener('click', (event) => {
-    if (document.querySelector('nav.menu').style.display == "none")
+    if (document.querySelector('.menu').style.display == "none")
     {
-        document.querySelector('nav.menu').style.display = "block";
+        document.querySelector('.menu').style.display = "block";
     }
     else
     {
-        document.querySelector('nav.menu').style.display = "none";
-
+        document.querySelector('.menu').style.display = "none";
     }
 })
